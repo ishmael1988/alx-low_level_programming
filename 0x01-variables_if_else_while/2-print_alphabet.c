4d04e5e4e1cc076952303896e468946337d23adf
ishmael1988 <ishmaelsesay155@gmail.com>
2022-12-10 13:51:26 -0800

    game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
  * main - prints alpha in lower case
- * Return: always 0
+ * Return: Always (0)
  */
 int main(void)
 {
 	char lc;
 
 	for (lc = 'a'; lc <= 'z'; lc++);
-	{
+	{	
 		putchar(lc);
 	}
-	putchar('\n');
+	puchar('\n');
 	return (0);
-}
+}	
