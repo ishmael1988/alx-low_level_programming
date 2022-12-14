30cd8ca477a57c57a40585e2b527744f1db186a9
ishmael1988 <ishmaelsesay155@gmail.com>
2022-12-14 02:12:14 -0800

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - print if the number positive,nagative or 0
+ * main - prints if the number is positive,negative or 0
  * Return: Always 0
  */
 int main(void)
@@ -10,23 +10,19 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX  2;
+	n = rand() - RAND_MAX / 2;
 
-	if (n < 0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
-
 	}
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
-			else if (n < 0)
-			{
-				printf("%d is agative\n" n);
-			}
-
+	else if (n < 0)
+	{
+		printf("%d is negative\n");
+	}
 	return (0);
 }
-
-
