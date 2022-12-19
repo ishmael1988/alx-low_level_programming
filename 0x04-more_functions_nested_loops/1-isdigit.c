c22dfa48fa05f7425c7a02dab91b1072f52c4e5c
ishmael1988 <ishmaelsesay155@gmail.com>
2022-12-18 17:28:01 -0800

    function in nested loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check if numbers are 0 - 0
+ * @c: char to check
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= '0' && <= '9'
+			return (1);
+	else
+	return (0);
+}
