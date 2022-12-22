43c64b378006880f585287651bb2945ca4744e21
ishmael1988 <ishmaelsesay155@gmail.com>
2022-12-22 02:20:30 -0800

    this is task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,21 +8,21 @@
  */
 char  *_strcat(char *dest, char *src)
 {
-	int 1;
+	int i;
 	int j;
 
 	i = 0;
 	while (dest[i] != '\0')
 	{
-		1++;
+		i++;
 	}
 	j = 0;
 	while (src[j] != '\0')
 	{
-		dest[1] = src[j];
-		!++;
+		dest[i] = src[j];
+		i++;
 		j++;
 	}
-	dest[!] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
