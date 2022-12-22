55e13fb359d6c79a2d4d65f16d8dffaf63b17c72
ishmael1988 <ishmaelsesay155@gmail.com>
2022-12-22 03:19:01 -0800

    were is the fult

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,7 +4,7 @@
  * _strncpy - copys a string with n
  * @dest: copy to
  * @src: copy from
- * @n: number of char to be copy
+ * @n: number of char to be copied
  * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n);
