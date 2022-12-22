b2fd804525409f5fea7acb2469cc5f4474a51d77
ishmael1988 <ishmaelsesay155@gmail.com>
2022-12-22 05:16:07 -0800

    Prepare for the worst

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,7 +19,7 @@ char *cap_string(char *str)
 			 str[i - 1] == '\n' ||
 			 str[i - 1] == ',' ||
 			 str[i - 1] == ';' ||
-			 str[i - 1] == ',' ||
+			 str[i - 1] == '.' ||
 			 str[i - 1] == '!' ||
 			 str[i - 1] == '?' ||
 			 str[i - 1] == '"' ||
