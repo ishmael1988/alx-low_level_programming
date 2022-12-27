276991cdf846f9be44ec629278568baac2f6fe38
ishmael1988 <ishmaelsesay155@gmail.com>
2022-12-27 06:15:51 -0800

    am back this time again

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,29 +1,34 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
 /**
- * main - Entry point
- *
- * Return: always 0 (success)
- */
+ * main - Entry
+ * Return: always 0
+*/
+
 int main(void)
 {
 	int n;
-
-	int m;
+	int a;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	m = n % 10;
-	if (m > 5)
-		pirntf("Last digit of %d is %d and is greater than 5\n", n, m);
-	if (m == 0)
-		printf("Last digit of %d is %d and is 0\n", n, m);
-	if (m < 6 && m != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+	a = n % 10;
+
+	if (a > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, a);
+	}
+
+	else if (a == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, a);
+	}
+
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
+	}
 	return (0);
 }
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-/**
- * main - prints alpha in lower case
- * Return: Always (0)
- */
+
 int main(void)
 {
-	char lc;
-
-	for (lc = 'a'; lc <= 'z'; lc++);
-	{	
-		putchar(lc);
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
 	}
-	puchar('\n');
-	return (0);
-}	
+
+	{
+		putchar('\n');
+
+		return 0;
+	}
+}
