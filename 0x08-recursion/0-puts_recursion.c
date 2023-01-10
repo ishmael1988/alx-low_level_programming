7fd859d9103941050c077aca0962a1157a879c10
ishmael1988 <ishmaelsesay155@gmail.com>
2023-01-10 10:59:46 -0800

    frustrating

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,12 +10,9 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		putchar('\n');
+		putchar(*s);
+		_puts_recursion(s + 1);
 	}
 	else
-	{
-		putchar(*s);
-		_puts_recursion((s + 1));
-	}
+		 putchar('\n');
 }
-
