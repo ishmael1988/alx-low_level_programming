448fc2984632fd7f19e05e1f72a0423da0506b75
ishmael1988 <ishmaelsesay155@gmail.com>
2023-01-10 12:41:02 -0800

    something she once knew

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,16 @@
 #include "main.h"
-
 /**
-  * _puts_recursion - Function that prints strings
-  * @s: Pointer to a string
-  * Return: Nothing
-  */
-
-void _puts_recursion(char *s)
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string
+ * Return: no return.
+ */
+void _puts_recursion(char *s);
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		putchar('\n');
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 	else
-	{
-		putchar(*s);
-		_puts_recursion((s + 1));
-	}
+		_putchar('\n');
 }
