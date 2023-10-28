5c44d31d46ba91b4e8d97f318c8bba95e3890f1c
root <you@example.com>
2023-10-28 02:12:58 -0700

    0-puts_recursion_.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
+/**
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 
-void _puts_recursion(char *s);
-int _putchar(char c);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
-int is_palindrome(char *s);
-#endif
-
+	else
+		_putchar('\n');
+}
 
